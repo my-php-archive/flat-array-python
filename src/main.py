9af7091c2663b81bceb0e8edcdf2b1bd656c4a99
Fernando Dorantes <fdorantesm@gmail.com>
2020-05-14 18:27:57 -0500

    Make isolated function

--- src/main.py
+++ src/main.py
@@ -1,18 +1,17 @@
-input = [1, [2, [3, [4, 5],[6,[7,[8, 9, 10, [11], [12], 13, 14, 15], 16], 17], 18, [19], 20], 21], 22, [23, [24, [25]]]]
-flatList = []
+def flatten(elements):
+    flatList = []
+    def flat(element):
+        if isinstance(element, list):
+            for subelement in element:
+                if isinstance(subelement, list):
+                    flat(subelement)
+                else:
+                    flatList.append(subelement)
+        else:
+            flatList.append(element)
+    flat(elements)
+    return flatList
 
-def flat(element):
-    if isinstance(element, list):
-        for subelement in element:
-            if isinstance(subelement, list):
-                flat(subelement)
-            else:
-                flatList.append(subelement)
-    else:
-        flatList.append(element)
-
-
-flat(input)
-
-print("input", input)
-print("flatten", flatList)
+print("flatten", flatten([1, [2, [3, [4, 5],[6,[7,[8, 9, 10, [11], [12], 13, 14, 15], 16], 17], 18, [19], 20], 21], 22, [23, [24, [25]]]]))
+print("flatten", flatten([1, 2, 3, 4, [5, 6], 7]))
+print("flatten", flatten([1, [2, [3, [4]]]]))
